a7b1e8dfdeee66a53ac14417a49c1b15c82a2b70
Paulo Henrique Maciel <paul.henrique583@gmail.com>
2020-08-04 11:13:27 -0300

    adding modal when a click occur

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:[20, 50, 80.50], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:[20, 50, 80.10], sizes:['320g', '640g', '1KGg'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:[20, 50, 80.99], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:[20, 50, 80.55], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:[20, 50, 80.19], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:[20, 50, 80.02], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:[20, 50, 80.09], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:[20.15, 50.95, 80.50], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:[20.25, 50.49, 80.10], sizes:['320g', '640g', '1KGg'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:[20.36, 50.42, 80.99], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:[20.47, 50.15, 80.55], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:[20.59, 50.98, 80.19], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:[20.14, 50.58, 80.02], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:[20.85, 50.99, 80.09], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
 ];
--- script.js
+++ script.js
@@ -3,15 +3,24 @@ const cs = (el) => document.querySelectorAll(el);
 
 pizzaJson.map((item, index)=>{
 
+    // preenche as informações em pizza item
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
-    // preencher as informações em pizza item
+ 
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[2].toFixed(2).replace('.', ',')}`;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[1].toFixed(2).replace('.', ',')}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=> {
+       e.preventDefault();
+       
 
- 
- 
+       c('.pizzaWindowArea').style.opacity = 0;
+       c('.pizzaWindowArea').style.display = 'flex';
+       setTimeout(() => {
+           c('.pizzaWindowArea').style.opacity = 1;
+       }, 200);
+
+    });
 
     c('.pizza-area').append(pizzaItem);
 });
