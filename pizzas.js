184aeb162213671ea089b56de86beafca0631c51
Paulo Henrique Maciel <paul.henrique583@gmail.com>
2020-08-04 10:34:57 -0300

    adding img, name, description and price to itens

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:[20, 50, 80], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:[20, 50, 80], sizes:['320g', '640g', '1KGg'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:[20, 50, 80], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:[20, 50, 80], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:[20, 50, 80], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:[20, 50, 80], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:[20, 50, 80], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:[20, 50, 80.50], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:[20, 50, 80.10], sizes:['320g', '640g', '1KGg'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:[20, 50, 80.99], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:[20, 50, 80.55], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:[20, 50, 80.19], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:[20, 50, 80.02], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:[20, 50, 80.09], sizes:['320g', '640g', '1KG'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
 ];
--- script.js
+++ script.js
@@ -5,6 +5,13 @@ pizzaJson.map((item, index)=>{
 
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     // preencher as informações em pizza item
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[2].toFixed(2).replace('.', ',')}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+ 
+ 
 
     c('.pizza-area').append(pizzaItem);
 });
