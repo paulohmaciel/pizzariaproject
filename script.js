cf8ff06ae70263c99db756618599ebb865075852
Paulo Henrique Maciel <paul.henrique583@gmail.com>
2020-08-05 09:17:36 -0300

    button plus and less

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let modalQt = 1;
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
+// Listagem das pizzas
 pizzaJson.map((item, index)=>{
 
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
@@ -39,3 +40,30 @@ pizzaJson.map((item, index)=>{
 
     c('.pizza-area').append(pizzaItem);
 });
+
+// Eventos do Modal
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+};
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if (modalQt > 1){
+    modalQt--;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=> {
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+ });
