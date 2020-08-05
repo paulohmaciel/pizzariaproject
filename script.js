bb9b433dc6546456889c4d426f317fe7679110df
Paulo Henrique Maciel <paul.henrique583@gmail.com>
2020-08-05 09:58:14 -0300

    addind cart push

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
@@ -17,6 +19,7 @@ pizzaJson.map((item, index)=>{
        e.preventDefault();
        let key = e.target.closest('.pizza-item').getAttribute('data-key');
        modalQt = 1;
+       modalKey = key;
 
        c('.pizzaBig img').src = pizzaJson[key].img;  
        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -66,4 +69,14 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
         c('.pizzaInfo--size.selected').classList.remove('selected');
         size.classList.add('selected');
     });
+ });
+ c('.pizzaInfo--addButton').addEventListener('click', () =>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+
+    cart.push({
+        id:pizzaJson[modalKey].id,
+        size,
+        qt:modalQt
+    });
+    closeModal();
  });
